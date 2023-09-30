67b98eed2f2c5ad5a70b176a8c01abecb1cdbacc
kanjunzhe <91109712+kanjunzhe@users.noreply.github.com>
2023-09-30 12:02:38 +0800

    v1.1.0

--- js/head.js
+++ js/head.js
@@ -1,8 +1,7 @@
 let head = document.querySelector(".head");
-let bg = document.querySelector(".bg");
 
 window.addEventListener("scroll", (event) => function () {
     var value = window.scrollY;
-    head.style.top = value + "px";
-    bg.style.top = value + "px";
+    // head.computedStyleMap.get("--scrollY").value = value + "px";
+    head.style.top = value * 1 + "px";
 });
